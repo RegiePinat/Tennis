033e76fedb5a3fe2124c0f6e740798aeaeaec21e
Charles Marlon Ramones <charles@klab.com>
2012-11-07 10:56:36 -0800

    added class
    hjhskahdsd

---
+++ StoryBoardPractice/StoryBoardPractice/MainViewControllerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainViewControllerAppDelegate.h
+//  StoryBoardPractice
+//
+//  Created by Charles Marlon G. Ramones on 11/7/12.
+//  Copyright (c) 2012 Charles Marlon G. Ramones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewControllerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryBoardPractice/StoryBoardPractice/MainViewControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewControllerViewController.h
+//  StoryBoardPractice
+//
+//  Created by Charles Marlon G. Ramones on 11/7/12.
+//  Copyright (c) 2012 Charles Marlon G. Ramones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewControllerViewController : UIViewController
+
+@end
